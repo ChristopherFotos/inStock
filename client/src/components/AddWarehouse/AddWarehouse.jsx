4dfb2a4bad24f8b35504da06c36e88b75b5ba683
Chris Johns <chrisfotosmusic@gmail.com>
2021-01-12 00:14:04 -0500

    add event handlers to EditWarehouse and AddWarehouse

--- client/src/components/AddWarehouse/AddWarehouse.jsx
+++ client/src/components/AddWarehouse/AddWarehouse.jsx
@@ -3,6 +3,37 @@ import arrow from '../../assets/images/Icons/arrow_back-24px.svg'
 import './AddWarehouse.scss'
 
 export default class AddWarehouse extends Component {
+    constructor(props){
+        super()
+        this.state = {
+            id: "2922c286-16cd-4d43-ab98-c79f698aeab0",
+            name: "Manhattan",
+            address: "503 Broadway",
+            city: "New York",
+            country: "USA",
+            contact: {
+              name: "Parmin Aujla",
+              position: "Warehouse Manager",
+              phone: "+1 (646) 123-1234",
+              email: "paujla@instock.com"
+            }
+        }
+    }
+
+    handleDetailChange(e){
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleContactChange(e){
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() {
         return (
             <div className = 'form'>
@@ -47,22 +78,22 @@ export default class AddWarehouse extends Component {
 
                         <div className="form__input-wrapper">
                             <label htmlFor="name" className="form__label">Warehouse Name</label>
-                            <input type="text" className='form__input'/>
+                            <input type="text" data-type='contact' className='form__input'/>
                         </div>
 
                         <div className="form__input-wrapper">
                             <label htmlFor="address" className="form__label">Street Address</label>
-                            <input type="text" className='form__input'/>
+                            <input type="text" data-type='contact' className='form__input'/>
                         </div>
 
                         <div className="form__input-wrapper">
                             <label htmlFor="city" className="form__label">City</label>
-                            <input type="text" className='form__input'/>
+                            <input type="text" data-type='contact' className='form__input'/>
                         </div>
 
                         <div className="form__input-wrapper">
                             <label htmlFor="country" className="form__label">Country</label>
-                            <input type="text" className='form__input'/>
+                            <input type="text" data-type='contact' className='form__input'/>
                         </div>
                     </div>
                 </div>
