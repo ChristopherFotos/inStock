186cac9c6254e126461d1ed8a9c7a278ec56dcb0
drake <tehtrojenguy@gmail.com>
2021-01-13 10:46:59 -0600

    added backend routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Switch>
+            <Route path="/">
+              <Redirect to="/warehouses"/> //redirects the user to the warehouses list
+            </Route>
+            <Route path="/warehouses" /> //displays a list of warehouses
+            <Route path="/inventory" /> //displays a list of inventory
+            <Route path="/warehouse/:id" /> //displays information on a specific warehouse
+            <Route path="/warehouse/:id/inventory" /> //displays a list of a specific warehouse's inventory
+            <Route path="/warehouse/:id/inventory/:itemid" /> //displays a specific item info in a specific warehouse
+          </Switch>
+        </Router>
     </div>
   );
 }
