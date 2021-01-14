25dc297fd4ec4363e1d1b0559a1975d6f2cb5d49
Chris Johns <chrisfotosmusic@gmail.com>
2021-01-13 22:02:43 -0500

    add helper functions for form validation. add route to post new warehouse

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -1,5 +1,6 @@
 const express    = require('express')
 const fs         = require('fs')
+const h          = require('../helper-functions')
 const {v4: uuid} = require('uuid');
 const cors      = require('cors')
 const router    = express.Router()
@@ -11,6 +12,27 @@ const warehouses      = require('../data/warehouses.json')
 router.get('/', (req, res)=>{
     res.json(warehouses)
 })
+
+/* add a new warehouse */
+router.post('/', (req, res)=>{
+    const warehouse = req.body
+
+    if(h.validateProperties(warehouse).length > 0){
+        res.status(400).json({
+            message:'your request was either missing some information, or included som invalid information',
+            incorrectProperties: h.validateProperties(warehouse)
+        })
+    }
+
+    let id = uuid()
+    warehouse.id = id
+    warehouses.push(warehouse)
+
+    fs.writeFile('../data/warehouses.json', warehouses, ()=>1)
+    let newWarehouse = warehouses.find(w => w.id === id)
+    res.json(newWarehouse)
+})
+
 
 module.exports = router
 
