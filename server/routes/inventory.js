193d0a6a980acf305d28a3eb882f9c49934416d3
Chris Johns <chrisfotosmusic@gmail.com>
2021-01-16 17:55:34 -0500

    add route to create new inventory item

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -1,5 +1,6 @@
 const express    = require('express')
 const fs         = require('fs')
+const h          = require('../helper-functions')
 const {v4: uuid} = require('uuid');
 const cors      = require('cors')
 const router    = express.Router()
@@ -15,6 +16,30 @@ router.get('/:id', (req,res)=>{
     if(inventory.length < 1) res.status(404).json({error: "We couldn't find any items associated with that warehouse ID"})
 
     res.json(inventory)
+})
+
+/* ADD NEW ITEM */
+router.post('/', (req, res)=>{
+    const item = req.body
+
+    // check for empty inputs
+    let invalidProperties = h.validateProperties(item)
+    if(invalidProperties.length > 0){
+        res.status(400).json({
+            message:'your request was either missing some information, or included some invalid information',
+            invalidProperties: invalidProperties
+        })
+    }
+
+    // assign an id and push into item array
+    let id  = uuid()
+    item.id = id
+    inventories.push(item)
+
+    // write to file and respond with the new item
+    fs.writeFile('./data/inventory.json', JSON.stringify(inventories), (err)=>console.log(err))
+    let newItem = inventories.find(w => w.id === id)
+    res.json(newItem)
 })
 
 module.exports = router
