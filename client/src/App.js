5295271bb0cbca8dd347d81178ce89f5c61193e0
Chris Johns <chrisfotosmusic@gmail.com>
2021-01-15 20:50:45 -0500

    add warehouse details component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import AddWarehouse from './components/AddWarehouse/AddWarehouse'
 import EditWarehouse from './components/EditWarehouse/EditWarehouse'
 import './App.css';
 import Header from './components/Header/Header';
 import Warehouselist from './components/Warehouselist/Warehouselist';
+import WarehouseDetails from './components/WarehouseDetails/WarehouseDetails'
 
 class App extends React.Component {
   
@@ -18,7 +19,7 @@ class App extends React.Component {
             <Route path="/warehouses" exact/> //displays a list of warehouses
             <Route path="/warehouses/add" component={AddWarehouse}/> //displays a list of warehouses
             <Route path="/inventory" /> //displays a list of inventory
-            <Route path="/warehouse/:id" exact/> //displays information on a specific warehouse
+            <Route path="/warehouse/:id" exact component={WarehouseDetails}/> //displays information on a specific warehouse
             <Route path="/warehouse/:id/edit" component={EditWarehouse}/> //displays the form to edit a particular warehouse
             <Route path="/warehouse/:id/inventory" /> //displays a list of a specific warehouse's inventory
             <Route path="/warehouse/:id/inventory/:itemid" /> //displays a specific item info in a specific warehouse
