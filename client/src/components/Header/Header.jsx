6a0871241ef819487d7230d14804c21dd01c0d4c
Mark Pelka <mark.pelka@gmail.com>
2021-01-16 13:45:46 -0500

    added routes to homepage on logo click, warehouses takes you to main page and add new warehouse button takes you to add warehouse page

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,10 +9,12 @@ function Header() {
       <nav>
         <div className="navbar">
           <div className="navbar__image">
-            <img src={logo} alt="Instock site logo"/>
+            <Link to='/'>
+              <img src={logo} alt="Instock site logo"/>
+            </Link>
           </div>
             <div className="navbar__container">
-              <Link to='/warehouses'><h3 className="navbar__container-active" >Warehouses</h3></Link>
+              <Link to='/'><h3 className="navbar__container-active" >Warehouses</h3></Link>
               <Link to='/inventory'><h3 className="navbar__container-link">Inventory</h3></Link>
             </div>
         </div>
