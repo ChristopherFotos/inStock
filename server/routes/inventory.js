4867f8447840ddb11de9c37b3fafb5be955cb720
drake <tehtrojenguy@gmail.com>
2021-01-17 14:21:33 -0600

    completed ticket

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -1,11 +1,20 @@
 const express    = require('express')
 const fs         = require('fs')
 const {v4: uuid} = require('uuid');
-const cors      = require('cors')
-const router    = express.Router()
-router.use(cors())
+const cors      = require('cors');
+const router    = express.Router();
+
+router.use(cors());
+
+
 /* import the data */
-const inventories      = require('../data/inventories.json')
-const warehouses      = require('../data/warehouses.json')
+const inventories = require('../data/inventories.json');
+const warehouses = require('../data/warehouses.json');
+
+router.route('/')
+    .get((req,res) => {
+        console.log("ROUTE YES");
+        res.json(inventories)
+    })
 
 module.exports = router
